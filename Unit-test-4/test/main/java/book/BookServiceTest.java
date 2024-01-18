52c9475e192c39303678bf1812596ad4cfcea29d
artketler <artketler.ak@gmail.com>
2024-01-18 18:31:22 +0300

    H_W_4 v 1.1 ok

--- Unit-test-4/test/main/java/book/BookServiceTest.java
+++ Unit-test-4/test/main/java/book/BookServiceTest.java
@@ -1,7 +1,41 @@
 package main.java.book;
 
+import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class BookServiceTest {
+    @Test
+    public void testFindBookById() {
+        BookRepository mockRepository = mock(BookRepository.class);
 
+        when(mockRepository.findById("111")).thenReturn(new Book("111", "Test Book", "Test Author"));
+
+        BookService bookService = new BookService(mockRepository);
+
+        Book result = bookService.findBookById("111");
+
+        assertEquals("Test Book", result.getTitle());
+        assertEquals("Test Author", result.getAuthor());
+    }
+
+    @Test
+    public void testFindAllBooks() {
+        BookRepository mockRepository = mock(BookRepository.class);
+
+        when(mockRepository.findAll()).thenReturn(List.of(new Book("111", "Test Book", "Test Author")));
+
+        BookService bookService = new BookService(mockRepository);
+
+        List<Book> result = bookService.findAllBooks();
+
+        assertEquals(1, result.size());
+        assertEquals("Test Book", result.get(0).getTitle());
+        assertEquals("Test Author", result.get(0).getAuthor());
+    }
 }
